98e611c47eea26eb6f66a5ec708f55fe9751d3c9
shassini <s.hassini@gmx.de>
2020-01-18 06:29:05 +0100

    doku fertig

--- Dokumentation/listings/kalorienbedarf.java
+++ Dokumentation/listings/kalorienbedarf.java
@@ -1,5 +1,9 @@
 public void getKalorienbedarf(){
     int grundumsatz = Math.round(66.47f + (13.2f + user.getWeight) + (5 * user.getHeight) - (6.8f * user.getAge));
-    int leistungsumsatz = Math.round(0.95f * user.getCalorieDegreeOne + 1.2f * user.getCalorieDegreeTwo + 1.45f * user.getCalorieDegreeThree + 1.65f * user.getCalorieDegreeFour + 1.85f * user.getCalorieDegreeFive + 2.2f * user.getCalorieDegreeSix);
-    int kalorienbedarf = grundumsatz + leistungsumsatz;
+
+    float anzahlStundenAktivitaet = user.getCalorieDegreeOne + user.getCalorieDegreeTwo + user.getCalorieDegreeThree + user.getCalorieDegreeFour + user.getCalorieDegreeFive + user.getCalorieDegreeSix;
+
+    int leistungsumsatz = Math.round((0.95f * user.getCalorieDegreeOne / anzahlStundenAktivitaet) + (1.2f * user.getCalorieDegreeTwo / anzahlStundenAktivitaet) + (1.45f * user.getCalorieDegreeThree / anzahlStundenAktivitaet) + (1.65f * user.getCalorieDegreeFour / anzahlStundenAktivitaet) + (1.85f * user.getCalorieDegreeFive / anzahlStundenAktivitaet) + (2.2f * user.getCalorieDegreeSix / anzahlStundenAktivitaet));
+
+    int kalorienbedarf = grundumsatz * leistungsumsatz;
 }
